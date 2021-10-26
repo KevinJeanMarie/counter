5057c26fd9bea41df05b11e3de257272534fb0fb
KevinJeanMarie <kjeanmarie01@gmail.com>
2021-10-26 14:18:19 +0200

    modif

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+class App extends React.Component {
+  constructor(){
+    super();
+this.state ={
+  count: 0
+};
+
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Counter</h1>
+        <h2> 0 </h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
